711a8d270214cb9f567ed7c9d52ca4a653f4047f
Valeriy Sobolev <vsobolev93@list.ru>
2022-08-09 21:42:15 +0400

    Добавил метод поработе с массивом

--- control_task/Program.cs
+++ control_task/Program.cs
@@ -9,3 +9,16 @@ void PrintArray (string[] array)
     }
     Console.Write ("]");
 }
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
