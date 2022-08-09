ad0eff398afe73b65517a6c98ce752810793c5a4
Valeriy Sobolev <vsobolev93@list.ru>
2022-08-09 21:41:28 +0400

    Добавил описывающий файл и схему написал метод вывода массива

---
+++ control_task/Program.cs
@@ -0,0 +1,11 @@
+﻿// Контрольная работа Соболев В.А.
+void PrintArray (string[] array)
+
+{
+    Console.Write ("Результат: [ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.Write ("]");
+}
